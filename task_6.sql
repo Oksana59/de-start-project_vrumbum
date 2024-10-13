9c538018d4d0be552614116b9956fcb1e36562ad
Oksana59 <oksana-pospelova@mail.ru>
2024-10-13 19:51:07 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,1 +1,6 @@
 /*добавьте сюда запрос для решения задания 6*/
+SELECT 
+COUNT(DISTINCT c.person_name) AS persons_from_usa_count
+FROM car_shop.sales s 
+JOIN car_shop.clients c ON c.id = s.person_name_id 
+WHERE c.phone LIKE '+1%';
