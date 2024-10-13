715eff9879fd1670f105dcdb904b675629d6b7cc
Oksana59 <oksana-pospelova@mail.ru>
2024-10-13 19:49:36 +0300

    Update task_1.sql

--- task_1.sql
+++ task_1.sql
@@ -1,1 +1,11 @@
 /*добавьте сюда запрос для решения задания 1*/
+SELECT
+(SELECT 
+count(*) AS total
+FROM car_shop.sales s
+LEFT JOIN car_shop.gasoline_consumptions gc ON gc.id = s.gasoline_consumption_id
+WHERE gc.id IS NULL)::real*100
+/
+(SELECT 
+count(*) AS total
+FROM car_shop.sales s) AS nulls_percentage_gasoline_consumption;
